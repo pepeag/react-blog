7ab0a404d920984de8203d2573aa14a83d1fccac
Phyo Phyo Aung <pepeag.1995@gmail.com>
2021-06-11 22:19:26 +0630

    react-blog

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const {data:blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
+
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
+
+  return (
+
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>loading ...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+}
+ 
+export default Home;
